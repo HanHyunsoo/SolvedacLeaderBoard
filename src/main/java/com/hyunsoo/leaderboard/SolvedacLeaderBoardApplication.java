ce5c965374d6b8a30726260fb6704a20d655998a
HanHyunSoo <gustn4563@gmail.com>
2022-05-20 09:29:56 +0900

    Start Project

---
+++ src/main/java/com/hyunsoo/leaderboard/SolvedacLeaderBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsoo.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvedacLeaderBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolvedacLeaderBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyunsoo/leaderboard/SolvedacLeaderBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunsoo.leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvedacLeaderBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
